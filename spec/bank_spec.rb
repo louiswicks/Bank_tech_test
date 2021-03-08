941326cb89013c90332b2a7acb162ec150ecc0cb
Louis Wickremeratne <louiswickremeratne@yahoo.co.uk>
2021-03-08 14:57:51 +0000

    Added testing and functionality for a withdraw method

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -11,12 +11,20 @@ describe BankAccount do
       expect(account.deposit(10)).to eq(10)
     end
 
-    it 'should increase account balance by amount' do
+    it 'should increase account balance by a specified amount' do
       account = BankAccount.new
       account.deposit(10)
       expect(account.balance).to eq(10)
-    end 
-
+    end
   end
+
+  describe '#withdraw' do
+      it 'should decrease account balance by a specified amount' do
+        account = BankAccount.new
+        account.deposit(10)
+        account.withdraw(3)
+        expect(account.balance).to eq(7)
+      end
+    end
 
 end
