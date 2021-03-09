f3636dbbf877820ae301daa6048944763f6195b8
Louis Wickremeratne <louiswickremeratne@yahoo.co.uk>
2021-03-09 15:42:15 +0000

    Improved transaction history test and test that was showing the same thing as another

--- lib/bank.rb
+++ lib/bank.rb
@@ -4,19 +4,32 @@ class BankAccount
   attr_reader :balance
 
   def initialize
+    @transactions = []
     @balance = 0
   end
 
   def deposit(amount)
     @balance += amount
+    @transactions << [Time.now.strftime("%d/%m/%Y"), amount, @balance]
   end
 
   def withdraw(amount)
     @balance -= amount
+    @transactions << [Time.now.strftime("%d/%m/%Y"), -amount, @balance]
   end
 
   def statement()
-    "Account balance: #{@balance}"
+    print_header
+    output = @transactions.reverse.map do |transaction|
+      puts "#{transaction[0]} || #{transaction[1]} || #{transaction[2]}"
+    end
+    puts output.join("")
+  end
+
+  private
+
+  def print_header()
+    puts "Date || Credit || Debit || Balance"
   end
 
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -6,10 +6,10 @@ describe BankAccount do
   end
 
   describe '#deposit' do
-    it 'should return an amount' do
-      account = BankAccount.new
-      expect(account.deposit(10)).to eq(10)
-    end
+    # it 'should return an amount' do
+    #   account = BankAccount.new
+    #   expect(account.deposit(10)).to eq(10)
+    # end
 
     it 'should increase account balance by a specified amount' do
       account = BankAccount.new
@@ -31,9 +31,15 @@ describe BankAccount do
     it 'should print a history of your banking activity' do
       account = BankAccount.new
       account.deposit(10)
+      account.deposit(10)
       account.withdraw(3)
-      expect(account.statement()).to include('7')
+      msg = "Date || Credit || Debit || Balance\n"\
+            "09/03/2021 || -3 || 17\n"\
+            "09/03/2021 || 10 || 20\n"\
+            "09/03/2021 || 10 || 10\n\n"
+      # expect(account.statement()).to include('Date || Credit || Debit || Balance')
+      expect { account.statement() }.to output(msg).to_stdout
     end
   end
-#Time.now.strftime("%k:%M on %d/%m/%Y")
+
 end
